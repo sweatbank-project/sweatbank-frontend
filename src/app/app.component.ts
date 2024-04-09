185dea27d2c29d5313056adbc7c254afea8af8d3
simzub <78310955+simzub@users.noreply.github.com>
2024-04-09 16:11:06 +0300

    add iniital login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, LoginComponent],
 })
 export class AppComponent {
   title = 'sweatbank';
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit(): void {
+    console.log(this.loginForm.value);
+    this.loginForm.reset();
+  }
+}
