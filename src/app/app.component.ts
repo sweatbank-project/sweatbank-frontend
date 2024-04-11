625bc50a43717ab6a17f161733e3b7e82169b8df
Gabija Sabeckyte <gabeckyte@gmail.com>
2024-04-11 14:30:32 +0300

    created a header for user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, HeaderComponent],
 })
 export class AppComponent {
   title = 'sweatbank';
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  userName = 'Name';
+  userSurname = 'Surname';
+
+  logout() {
+  }
+
+  changeLanguage() {}
+}
