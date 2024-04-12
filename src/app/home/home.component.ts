862485623178d7e6b20ffb60d4984976e18c4434
Deividas <deividaspapuska@gmail.com>
2024-04-12 13:53:40 +0300

    add part of the script

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  @ViewChildren('activeStep') activeSteps!: QueryList<ElementRef>;
+  @ViewChildren('activeStepSection') activeStepsSection!: QueryList<ElementRef>;
 
+  navigateToStep(stepNumber: number): void {
+
+
+
+
+
+
+
+
+
+
+    this.activeSteps.forEach((stepElement: ElementRef) => {
+      const nativeElement = stepElement.nativeElement as HTMLElement;
+      const stepAttribute = parseInt(nativeElement.getAttribute('step') || '0', 10);
+      if (stepNumber > stepAttribute) {
+        nativeElement.classList.add('active');
+      } else {
+        nativeElement.classList.remove('active');
+        nativeElement.classList.remove('current');
+      }
+    });
+
+    this.activeStepsSection.forEach((stepElement: ElementRef) => {
+      const nativeElement = stepElement.nativeElement as HTMLElement;
+      const stepAttribute = parseInt(nativeElement.getAttribute('step') || '0', 10);
+      if (stepNumber === stepAttribute) {
+        nativeElement.classList.remove('d-none');
+      } else {
+        nativeElement.classList.add('d-none');
+      }
+    });
+  }
 }
