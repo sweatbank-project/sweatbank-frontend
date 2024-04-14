68c4b330a6da913cbd0c59a97a5fa70184e0a81c
Deividas <deividaspapuska@gmail.com>
2024-04-14 18:31:23 +0300

    add vehicle data, current tab collor, implemented vehicle selection(make, model)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { CarData, CarModel, carData } from './data';
 import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
@@ -11,21 +12,28 @@ export class HomeComponent {
   @ViewChildren('activeStep') activeSteps!: QueryList<ElementRef>;
   @ViewChildren('activeStepSection') activeStepsSection!: QueryList<ElementRef>;
 
+  carData: CarData = carData;
+  selectedMake: CarModel | null = null;
+
+  onMakeSelect(event: any) {
+    const make = event.target.value;
+    if (make) {
+      this.selectedMake =
+        this.carData.car_makes.find((car) => car.make === make) || null;
+    }
+  }
+
   navigateToStep(stepNumber: number): void {
-
-
-
-
-
-
-
-
-
-
     this.activeSteps.forEach((stepElement: ElementRef) => {
       const nativeElement = stepElement.nativeElement as HTMLElement;
       const stepAttribute = parseInt(nativeElement.getAttribute('step') || '0', 10);
-      if (stepNumber > stepAttribute) {
+
+      if (stepNumber === stepAttribute) {
+        console.log(nativeElement);
+        nativeElement.classList.add('current');
+        nativeElement.classList.remove('active');
+      } else if (stepNumber > stepAttribute) {
+        nativeElement.classList.remove('current');
         nativeElement.classList.add('active');
       } else {
         nativeElement.classList.remove('active');
