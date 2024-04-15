9d8dbf5298d00b1d29c4cf2765234c9a1c83074e
Gabija Sabeckyte <gabeckyte@gmail.com>
2024-04-15 13:14:21 +0300

    created header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import {HeaderComponent} from "./header/header.component";
 import {HomeComponent} from "./home/home.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, LoginComponent, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, LoginComponent, HeaderComponent, HomeComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'sweatbank';
