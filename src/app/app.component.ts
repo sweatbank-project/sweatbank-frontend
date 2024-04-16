1e8732cdfebca60f50053239f19de474714952eb
simzub <78310955+simzub@users.noreply.github.com>
2024-04-16 09:12:02 +0300

    Implement login UI with backend integration and authentication functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import {HeaderComponent} from "./header/header.component";
-import {HomeComponent} from "./home/home.component";
-import {FooterComponent} from "./footer/footer.component";
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, LoginComponent, HeaderComponent, HomeComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    LoginComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+  ],
 })
 export class AppComponent {
   title = 'sweatbank';
