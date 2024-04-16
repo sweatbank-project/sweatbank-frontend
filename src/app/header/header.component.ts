a580dc8e4b3fc9778b9afe1652f309d7ced6b790
Gabija Sabeckyte <gabeckyte@gmail.com>
2024-04-16 22:06:00 +0300

    completed header and footer corrections

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,41 +1,65 @@
-import { ChangeDetectorRef, Component, HostListener } from '@angular/core';
-import { faArrowRightFromBracket, faComments, faBell, faLanguage, faBars } from '@fortawesome/free-solid-svg-icons';
+import { Component, ViewChild, ElementRef, AfterViewInit, HostListener } from '@angular/core';
+import { faArrowRightFromBracket, faComments, faBell, faBars } from '@fortawesome/free-solid-svg-icons';
+import {Router, RouterLink} from '@angular/router';
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { CommonModule, NgClass } from "@angular/common";
-import { RouterLink, Router } from '@angular/router';
+
+// Assuming Bootstrap types are available (as an example)
+interface BootstrapCollapse {
+  show(): void;
+  hide(): void;
+}
+
+declare var bootstrap: {
+  Collapse: new (element: HTMLElement, options?: any) => BootstrapCollapse
+};
 
 @Component({
   selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
   standalone: true,
   imports: [FontAwesomeModule, NgClass, CommonModule, RouterLink],
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+  @ViewChild('menu') menuElement: ElementRef<HTMLElement> | undefined;
   menuActive: boolean = false;
   userName = 'Name';
   userSurname = 'Surname';
   faArrowRightFromBracket = faArrowRightFromBracket;
   faComments = faComments;
   faBell = faBell;
-  faLanguage = faLanguage;
   faBars = faBars;
+  private bsCollapse: BootstrapCollapse | null = null;
 
   constructor(private router: Router) {}
 
+  ngAfterViewInit() {
+    if (this.menuElement) {
+      this.bsCollapse = new bootstrap.Collapse(this.menuElement.nativeElement, {
+        toggle: false
+      });
+    }
+  }
+
   toggleMenu(): void {
     this.menuActive = !this.menuActive;
+    if (this.menuActive) {
+      this.bsCollapse?.show();
+    } else {
+      this.bsCollapse?.hide();
+    }
   }
 
   navigateAndScroll(): void {
     if (this.router.url !== '/') {
       this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
         this.scrollToBottom();
-        this.toggleMenu(); // Optionally close the menu
+        this.toggleMenu();
       });
     } else {
       this.scrollToBottom();
-      this.toggleMenu(); // Optionally close the menu
+      this.toggleMenu();
     }
   }
 
@@ -54,9 +78,5 @@ export class HeaderComponent {
     if (this.menuActive) {
       this.toggleMenu();
     }
-  }
-
-  changeLanguage(): void {
-    // Language change logic here
   }
 }
