d4ba0577f6cdac80a18d5f961cb099176ba8295a
dzeivis <deividaspapuska@gmail.com>
2024-04-21 21:55:14 +0300

    add: tables in admin applications and inbox pages

--- src/app/components/admin/inbox/inbox.component.ts
+++ src/app/components/admin/inbox/inbox.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {NgClass} from "@angular/common";
 import {RouterLink} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-inbox',
@@ -17,5 +18,21 @@ export class InboxComponent {
 
   toggleSidebar() {
     this.isClosed = !this.isClosed;
+  }
+
+  data:any;
+  constructor(private http: HttpClient){
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe(data => {
+
+      this.data = data;
+      setTimeout(()=>{
+        $('#applications').DataTable( {
+          pagingType: 'full_numbers',
+          pageLength: 10,
+          processing: true,
+          lengthMenu : [10, 25, 50],
+        } );
+      }, 1);
+    }, error => console.error(error));
   }
 }
