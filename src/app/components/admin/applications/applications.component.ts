4f54b0187bd9f30ab4521add85fe1040ec4bc757
dzeivis <deividaspapuska@gmail.com>
2024-04-24 16:04:20 +0300

    fix: table styles fix

--- src/app/components/admin/applications/applications.component.ts
+++ src/app/components/admin/applications/applications.component.ts
@@ -17,11 +17,11 @@ import {DataTablesModule} from 'angular-datatables';
 })
 export class ApplicationsComponent {
   isClosed: boolean = false;
-  
+
   dropdownStates: { [key: string]: boolean } = {
     dropdown1: false,
   };
-  
+
   openEmailForm(email: string) {
     this.router.navigate(['/admin/inbox', { email: email }]);
   }
@@ -29,7 +29,6 @@ export class ApplicationsComponent {
   toggleSidebar() {
     this.isClosed = !this.isClosed;
   }
-
   toggleDropdown(dropdownName: string) {
     this.dropdownStates[dropdownName] = !this.dropdownStates[dropdownName];
   }
@@ -37,33 +36,40 @@ export class ApplicationsComponent {
   data:any;
   constructor(private http: HttpClient, private router: Router) {
     this.http.get('http://localhost:8080/api/admin/leases').subscribe(data => {
+
       this.data = data;
-      setTimeout(() => {
-        $('#applications').DataTable({
+      setTimeout(()=>{
+        $('#applications').DataTable( {
           pagingType: 'full_numbers',
           pageLength: 10,
           processing: true,
           lengthMenu : [10, 25, 50],
         });
-        
+
         applyStylesToElements()
       }, 1);
     }, error => console.error(error));
 
     function applyStylesToElements() {
+      console.log('aaaa ')
       const styleProperties = {
         backgroundColor: "#FFF",
         margin: "5px"
       };
 
-      const tableElements = ['dt-length-0', 'dt-search-0'];
-      
-      tableElements.forEach(id => {
-        const element = document.getElementById(id) as HTMLInputElement;
-        Object.assign(element.style, styleProperties);
-      });
-      
-      const tableElement = document.getElementById('applications') as HTMLInputElement;
-      tableElement.style.width = '';
+      const elements = document.getElementsByClassName("dt-input");
+
+      for (let i = 0; i < elements.length; i++) {
+          const element = elements[i] as HTMLElement;
+          Object.assign(element.style, styleProperties);
+      }
+
+      const tableElement = document.getElementById('applications');
+      if (tableElement) {
+        tableElement.style.width = '';
+      } else {
+        console.error(`Element with ID 'applications' not found.`);
+      }
     }
+  }
 }
