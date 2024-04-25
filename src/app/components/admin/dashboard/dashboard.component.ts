32c4b490b49b5537ae4c7b56408b7d5a8bfbe9e2
dzeivis <deividaspapuska@gmail.com>
2024-04-25 23:08:28 +0300

    add: admin dashboard charts

--- src/app/components/admin/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {NgClass} from "@angular/common";
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,12 +15,42 @@ import {NgClass} from "@angular/common";
 })
 export class DashboardComponent {
   isClosed: boolean = false;
+  chart: any = [];
 
   data= {
     "new": 1,
     "pending": 2,
     "approved": 3,
     "rejected": 4
+  }
+
+  ngOnInit() {
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+        datasets: [
+          {
+            label: 'New Applications',
+            data: [12, 19, 3, 5, 2, 3, 20],
+            borderWidth: 1,
+          },
+          {
+            label: 'Admin Actions',
+            data: [6, 10, 5, 8, 1, 2, 10],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
   }
 
   toggleSidebar() {
